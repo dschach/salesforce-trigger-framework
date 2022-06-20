9a3998cacdd337fa08225bd16cb82d1f74bd71ec
David Schach <dschach@x2od.com>
2022-06-20 13:46:16 -0700

    Auto-convert README markdown to ApexDox home

--- scripts/updateHighlight.sh
+++ scripts/updateHighlight.sh
@@ -3,3 +3,8 @@
 curl -L "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js" -o "doc-assets/highlight.js"
 echo >> "assets/highlight.js"
 curl -L "https://cdn.jsdelivr.net/npm/highlightjs-apex/dist/apex.min.js" >> "doc-assets/highlight.js"
+
+npx marked -i README.md -o doc-assets/homePage.html smartypants=true
+echo >> "doc-assets/homePage.html"
+printf '<link href="assets/styling.css" rel="stylesheet" />' >> "doc-assets/homePage.html"
+npx prettier --write "doc-assets/homePage.html"
